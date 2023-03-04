d0db0a312a003fd7c3795e18c9027c43b643d6be
Manuel Rueda <luis.rueda@bizglo.com>
2023-03-04 15:15:55 -0500

    Added settings for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_BASE_URL = process.env.REACT_APP_API_URL 
+
 export const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: API_BASE_URL,
     withCredentials: true
 })
 
